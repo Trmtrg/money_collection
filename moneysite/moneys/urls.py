e4aeeea3a33ae54fbce7f19aa5f21f6c4e97c013
Иван <v.p204@mail.ru>
2023-03-19 00:53:09 +0300

    форма добавления записей

--- moneysite/moneys/urls.py
+++ moneysite/moneys/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.moneys_home, name='moneys_home')
+    path('', views.moneys_home, name='moneys_home'),
+    path('create', views.create, name='create')
 ]
