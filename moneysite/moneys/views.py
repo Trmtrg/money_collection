a4139dacad7301e7b382ea0e857229c53c11e07f
Иван <v.p204@mail.ru>
2023-03-20 00:36:28 +0300

    динамически изменяемые страницы

--- moneysite/moneys/views.py
+++ moneysite/moneys/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Article
 from .forms import ArticleForm
-
+from django.views.generic import DetailView
 
 def moneys_home(request):
     moneys = Article.objects.all()
     return render(request, 'moneys/moneys_home.html', {'moneys': moneys})
+
+
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = 'moneys/details_view.html'
+    context_object_name = 'Article'
 
 
 def create(request):
