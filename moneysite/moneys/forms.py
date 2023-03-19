568661e90c5a6b8e0861366c2bec4691019e5372
Иван <v.p204@mail.ru>
2023-03-19 23:47:46 +0300

    форма добавления записей

---
+++ moneysite/moneys/forms.py
@@ -0,0 +1,46 @@
+from .models import Article
+from django.forms import ModelForm, TextInput, FileInput, NumberInput
+from django.db.models import ImageField
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['Title', 'Image', 'Image_two', 'Country', 'Metal', 'Year', 'Nominal', 'Text']
+
+        widgets = {
+            "Title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название'
+            }),
+            "Image": FileInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Фото лицевой стороны монеты',
+                'accept': 'image/png'
+            }),
+            "Image_two": FileInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Фото обратной стороны монеты',
+                'accept': 'image/png'
+            }),
+            "Country": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Страна'
+            }),
+            "Metal": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Метал'
+            }),
+            "Year": NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Год выпуска монеты'
+            }),
+            "Nominal": NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Номинал'
+            }),
+            "Text": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Описание'
+            }),
+
+        }
