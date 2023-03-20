807001df368303b058862d66ab74eef61d961f8a
dubino <iliad007@mail.ru>
2023-03-21 01:33:55 +0300

    css changes

--- moneysite/main/urls.py
+++ moneysite/main/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('home/', views.index, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('details_view/', views.details_view, name='details_view')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
