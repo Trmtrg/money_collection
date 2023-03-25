b67479132203d4f34223d6e470c146ddfc7b1113
Trmtrg <vitalik.petrackov@yandex.ru>
2023-03-25 23:28:32 +0300

    Зачем-то поправил авторизацию

--- moneysite/user/forms.py
+++ moneysite/user/forms.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(label="Адрес эелектронной почты")
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
